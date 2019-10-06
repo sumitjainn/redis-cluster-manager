1c1c75351e04af40dcc58ea29627290e4030f602
Sumit Jain <sumjain@adobe.com>
2019-10-06 16:23:24 +0530

    Redis cluster script

---
+++ cluster.sh
@@ -0,0 +1,114 @@
+
+#!/bin/bash
+
+REDISBIN="/Users/sumjain/Software/final/redis-cluster"
+NODES_DIR=nodes
+#Usage: ./cluster.sh start|create|stop -h=192.168.1.7 -p=30000 -n=3 -r=1
+
+INDEX=0
+for i in "$@"
+do
+if [ "$INDEX" == "0" ]; then 
+    COMMAND="$i"
+    #echo "$INDEX : $i"
+else    
+    case $i in
+        -h=*|--host=*)
+        HOST="${i#*=}"
+        ;;
+        -p=*|--port=*)
+        PORT="${i#*=}"
+        ;;
+        -n=*|--num-nodes=*)
+        COUNT="${i#*=}"
+        ;;
+        -r=*|--replicas=*)
+        REPLICAS="${i#*=}"
+        ;;
+        --default)
+        DEFAULT=YES
+        ;;
+        *)
+               # unknown option
+        ;;
+    esac
+fi
+let INDEX=${INDEX}+1
+done
+
+MINNODES=$(( (REPLICAS+1) * 3 )) 
+
+if [[ ( "$COUNT" -lt 3 ) || ( "$COUNT" -lt "$MINNODES" ) ]]; then
+    echo "Count should be minimum $MINNODES for REPLICAS: $REPLICAS  ..exiting"
+    exit 1
+fi
+
+echo "command: $COMMAND"
+echo "host: $HOST"
+echo "start port: $PORT"
+echo "count: $COUNT"
+echo "replicas: $REPLICAS"
+
+#exit 0
+#set -x
+echo "Executing $COMMAND"
+
+if [ "$COMMAND" == "create" ]; then
+    mkdir -p $NODES_DIR 
+    cd $NODES_DIR; ls
+    CURR=0
+    CLUSTERSTR=""
+    while [ $((CURR < COUNT)) != "0" ]; do
+        DIR=$((PORT+CURR))
+        mkdir $DIR
+        cd $DIR ; pwd
+        sed 's/${PORT}/'$DIR'/; s/${HOST}/'$HOST'/;' ../../redis.conf.tmpl > redis.conf
+        ls
+        CLUSTERSTR="$CLUSTERSTR $HOST:$DIR"
+        #echo "$REDISBIN/redis-server redis.conf"
+        $REDISBIN/redis-server redis.conf
+        cd .. 
+        CURR=$(( CURR + 1 )) 
+    done
+    ps -ef | grep redis
+    CLUSTERCMD="$REDISBIN/redis-cli --cluster create $CLUSTERSTR --cluster-replicas $REPLICAS"
+    echo $CLUSTERCMD
+    $CLUSTERCMD
+    tail -f $NODES_DIR/**/*.log
+    exit 0
+fi    
+
+if [ "$COMMAND" == "start" ]; then
+    cd $NODES_DIR
+    CURR=0
+    while [ $((CURR < COUNT)) != "0" ]; do
+        DIR=$((PORT+CURR))
+        cd $DIR ; pwd
+        rm redis.log
+        ls
+        CMDD="$REDISBIN/redis-server redis.conf"
+        echo "`pwd` $CMDD"
+        $CMDD
+        cd .. 
+        CURR=$(( CURR + 1 )) 
+        done
+    cd ..    
+    ps -ef | grep redis    
+    tail -f $NODES_DIR/**/*.log
+    exit 0
+fi    
+
+if [ "$COMMAND" == "stop" ]; then
+    ps -ef | grep redis   
+    CURR=0
+    while [ $((CURR < COUNT)) != "0" ]; do
+        DIR=$((PORT+CURR))
+        pkill ":$DIR \[cluster\]"
+        CURR=$(( CURR + 1 )) 
+        done
+    cd ..    
+    ps -ef | grep redis    
+    exit 0
+fi    
+
+
